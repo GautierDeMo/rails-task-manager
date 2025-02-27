705b44f56a5ad4dc2b8f0d781254cf18d82e83db
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-02-27 10:41:49 +0100

    exo 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # raise
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # raise
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    # raise
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
